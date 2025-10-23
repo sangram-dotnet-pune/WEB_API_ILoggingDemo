9e4bea0ea8d1489d0342361ae446baacb6162eac
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2025-10-23 11:46:33 +0530

    Added Serilog Demo

---
+++ WEB_API_LoggingDemo/Controllers/SerilogDemoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API_LoggingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SerilogDemoController : ControllerBase
+    {
+
+        private readonly ILogger<SerilogDemoController> _logger;
+
+        public SerilogDemoController(ILogger<SerilogDemoController> logs)
+        {
+            _logger = logs;
+        }
+
+
+        [HttpGet("check")]
+        public IActionResult CheckLogging()
+        {
+            _logger.LogInformation("CheckLogging endpoint hit at {time}", DateTime.Now);
+
+            try
+            {
+                int a = 10;
+                int b = 2;
+                int c = a / b; // Generates DivideByZeroException
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An exception occurred while dividing numbers.");
+                return StatusCode(500, "Internal Server Error");
+            }
+
+            return Ok("Logging test successful!");
+        }
+    }
+}
--- WEB_API_LoggingDemo/Program.cs
+++ WEB_API_LoggingDemo/Program.cs
@@ -1,4 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// --- Configure Serilog ---
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug() // You can set to Information, Warning, etc.
+    .WriteTo.Console() // Logs to Console
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day) // Daily log files
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
 // Add services to the container.
 
@@ -6,6 +16,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
 builder.Logging.AddConsole();
 var app = builder.Build();
 
